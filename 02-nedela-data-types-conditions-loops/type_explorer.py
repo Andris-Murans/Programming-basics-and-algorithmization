cfce359cbb1f9cdccc7ab4c1679927e2757de396
Andris Murans <Andris.Murans@fita.lv>
2026-03-09 22:07:23 +0200

    Konsoles izvade ar katra mainīgā tipu

--- 02-nedela-data-types-conditions-loops/type_explorer.py
+++ 02-nedela-data-types-conditions-loops/type_explorer.py
@@ -20,3 +20,15 @@ age = 43             # int
 height = 1.75        # float
 is_student = False   # bool
 data = None          # NoneType
+
+# ------------------------------------------
+# 2. KONSOLES IZVADE AR KATRA MAINĪGĀ TIPU
+# ------------------------------------------
+
+print("1. MAINĪGIE UN TO TIPI\n")
+
+print(f"name = {name} -> {type(name)}") # f-string (formatēta virkne) ļauj ievietot mainīgos vai izteiksmes tieši tekstā, izmantojot {}
+print(f"age = {age} -> {type(age)}")
+print(f"height = {height} -> {type(height)}")
+print(f"is_student = {is_student} -> {type(is_student)}")
+print(f"data = {data} -> {type(data)}")
