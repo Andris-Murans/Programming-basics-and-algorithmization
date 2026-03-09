1fbb34c217bf64745e4b3800577f3637db9a7ff1
Andris Murans <Andris.Murans@fita.lv>
2026-03-09 21:44:25 +0200

    Mainīgo definēšana, izmantojot Python pamata datu tipus

---
+++ 02-nedela-data-types-conditions-loops/type_explorer.py
@@ -0,0 +1,22 @@
+"""
+type_explorer.py
+
+Šī programma demonstrē:
+- Python pamata datu tipus (int, float, str, bool, None)
+- mainīgo tipu noteikšanu ar type()
+- truthy / falsy uzvedību
+- datu tipu konversijas
+- robežgadījumus (ValueError)
+
+"""
+print("\n=== Uzdevums 1: Tipu pētnieks ===\n")
+
+# ------------------------------------------
+# 1. MAINĪGO DEFINĒŠANA
+# ------------------------------------------
+
+name = "Andris"      # str
+age = 43             # int
+height = 1.75        # float
+is_student = False   # bool
+data = None          # NoneType
