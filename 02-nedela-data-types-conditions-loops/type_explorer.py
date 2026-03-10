402460673c183168ca43f070cf7034b46d4d9451
Andris Murans <Andris.Murans@fita.lv>
2026-03-10 22:29:07 +0200

    feat: add explicit conversion

--- 02-nedela-data-types-conditions-loops/type_explorer.py
+++ 02-nedela-data-types-conditions-loops/type_explorer.py
@@ -38,6 +38,7 @@ print(f"data = {data} -> {type(data)}")
 # ------------------------------------------
 
 print("\n2. TRUTHY / FALSY PIEMĒRI\n")
+
 print(bool(False))      # Loģiskā vērtība False
 print(bool(0))          # Nulle Pythonā ir falsy
 print(bool(0.0))        # Peldošā punkta nulle ir falsy
@@ -50,3 +51,37 @@ print(bool(" "))        # Atstarpe ir simbols, ir truthy
 print(bool([0]))        # Saraksts nav tukšs, ir truthy
 print(bool(-1))         # Nenulles skaitlis ir truthy
 print(bool("Python"))   # Netukša virkne ir truthy
+
+# ------------------------------------------
+# 4. DATU TIPU PĀRVEIDOŠANA
+# ------------------------------------------
+
+
+print("\n3. DATU TIPU PĀRVEIDOŠANA\n")
+
+# Mainīgo definēšana
+text_integer_number = "3"
+text_decimal_number = "3.14"
+
+print("1) string → int\n")
+# Pārvēršam tekstu par skaitli
+integer_number = int(text_integer_number)   # int() pārvērš tekstu ar skaitli par veselu skaitli
+print(f"text_integer_number = {text_integer_number} -> {type(text_integer_number)}")
+print(f"number = {integer_number} -> {type(integer_number)}")
+
+print("\n2) string → float\n")
+# Pārvēršam tekstu par daļskaitli
+decimal_number = float(text_decimal_number)   # float() pārvērš tekstu par decimālskaitli
+print(f"text_decimal_number = {text_decimal_number} -> {type(text_decimal_number)}")
+print(f"number = {decimal_number} -> {type(decimal_number)}")
+
+print("\n3) float → int\n")
+# Pārvēršam daļskaitli par veselu skaitli
+number= int(decimal_number)   # int() neapaļo, tas nogriež decimāldaļu
+print(f"decimal_number = {decimal_number} -> {type(decimal_number)}")
+print(f"number = {number} -> {type(number)}")
+
+print("\n4) Zinātniskais pieraksts\n")
+# 1000.0 — "1e3" ir zinātniskais pieraksts (1 × 10³), Python to pārvērš par float
+print("1e3 = 1 × 10³ = 1000")
+print(float("1e3"))
