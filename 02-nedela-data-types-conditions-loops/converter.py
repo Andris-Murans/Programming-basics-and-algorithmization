9e5ed5aab68d236c8c25ca13ae09623aa1197f0d
Andris Murans <Andris.Murans@fita.lv>
2026-03-14 10:58:03 +0200

    feat: add constants for unit converter

---
+++ 02-nedela-data-types-conditions-loops/converter.py
@@ -0,0 +1,12 @@
+# converter.py
+
+# -----------------------------------
+# 1. Definējam konversijas konstantes
+# -----------------------------------
+# Konstantes raksta ar LIELAJIEM BURTIEM.
+# Tās ir vērtības, kuras programmā nemainās.
+
+KM_TO_MI = 0.621371      # 1 kilometrs = 0.621371 jūdzes
+KG_TO_LB = 2.20462       # 1 kilograms = 2.20462 mārciņas
+L_TO_GAL = 0.264172      # 1 litrs = 0.264172 galoni
+USD_TO_EUR = 0.84235020  # 1 dolārs = 0.84235020 eiro
