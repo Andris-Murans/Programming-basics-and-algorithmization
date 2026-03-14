dc7e6a8891a26626cc5ff3f403667046f0a53f31
Andris Murans <Andris.Murans@fita.lv>
2026-03-14 11:05:46 +0200

    feat: add and show the user a menu for conversion

--- 02-nedela-data-types-conditions-loops/converter.py
+++ 02-nedela-data-types-conditions-loops/converter.py
@@ -10,3 +10,15 @@ KM_TO_MI = 0.621371      # 1 kilometrs = 0.621371 jūdzes
 KG_TO_LB = 2.20462       # 1 kilograms = 2.20462 mārciņas
 L_TO_GAL = 0.264172      # 1 litrs = 0.264172 galoni
 USD_TO_EUR = 0.84235020  # 1 dolārs = 0.84235020 eiro
+
+# -----------------------------------
+# 2. Parādām lietotājam izvēlni
+# -----------------------------------
+print("Izvēlies konversiju:")
+print("1) km <-> mi")       # no km uz jūdzēm
+print("2) kg <-> lb")       # no kg uz mārciņām
+print("3) L <-> gal")       # no litriem uz galoniem
+print("4) $ <-> €")         # no dolāriem uz eiro
+
+# input() nolasa lietotāja ievadi no tastatūras
+choice = input("> ")
