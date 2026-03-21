c82cd911bf621f4d038697776afbbbde4fbc44a7
Andris Murans <Andris.Murans@fita.lv>
2026-03-21 08:15:30 +0200

    feat: create data_collections.py with basic structure

---
+++ week-03/src/data_collections.py
@@ -0,0 +1,8 @@
+# =========================
+# A DAĻA — SARAKSTI (list)
+# =========================
+
+print("--- Saraksti ---")
+
+# 1. Izveidojam sarakstu ar vismaz 5 skaitļiem
+numbers = [1, 2, 3, 4, 5]
